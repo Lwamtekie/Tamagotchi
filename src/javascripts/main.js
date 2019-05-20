ca31ad9a4b9b40e41c44d89b03c3422019e731c9
Lwam Tekie <ltekie87@yahoo.com>
2019-05-20 16:36:33 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,16 @@
 import '../styles/main.scss';
+import 'bootstrap';
+import eat from './components/eat';
+import play from './components/play';
+import fight from './components/fight';
+import sleep from './components/sleep';
+import pet from './components/pet';
 
-console.error('hi');
+const init = () => {
+  eat.domStringBuilder();
+  play.domStringBuilder();
+  fight.domStringBuilder();
+  sleep.domStringBuilder();
+  pet.domStringBuilder();
+};
+init();
