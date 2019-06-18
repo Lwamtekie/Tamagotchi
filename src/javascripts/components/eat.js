b9dda89c05358f86ea4817e7e3b51c7e3223037f
Lwam Tekie <ltekie87@yahoo.com>
2019-06-18 14:19:06 -0500

    buttons work

--- src/javascripts/components/eat.js
+++ src/javascripts/components/eat.js
@@ -1,1 +1,39 @@
 import util from '../helpers/util';
+
+
+let domString = '';
+const full = [100];
+
+function getSum(total, push) {
+  return total + push;
+}
+
+const healthyEats = () => {
+  if (full.reduce(getSum) < 91) {
+    full.push(10);
+    full.reduce(getSum);
+    const totalHealth = full.reduce(getSum);
+    document.getElementById('full').innerHTML = totalHealth;
+  }
+};
+
+const unHealthyEats = () => {
+  if (full.reduce(getSum) > 2) {
+    full.push(-3);
+    full.reduce(getSum);
+    const totalHealth = full.reduce(getSum);
+    document.getElementById('full').innerHTML = totalHealth;
+  }
+};
+
+const domStringBuilder = () => {
+  domString += '<button type="button" id="healthyFood">Eat good</button>';
+  domString += '<button type="button" id="unhealthyFood">Eat bad</button>';
+  domString += `<div id="full">${full}</div>`;
+  util.printToDom('eat', domString);
+  document.getElementById('healthyFood').addEventListener('click', healthyEats, false);
+  document.getElementById('unhealthyFood').addEventListener('click', unHealthyEats);
+  util.printToDom('pet', '<img src="../../../src/img/img.jpg">');
+};
+
+export default { domStringBuilder };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,13 +4,11 @@ import eat from './components/eat';
 import play from './components/play';
 import fight from './components/fight';
 import sleep from './components/sleep';
-import pet from './components/pet';
 
 const init = () => {
   eat.domStringBuilder();
   play.domStringBuilder();
   fight.domStringBuilder();
   sleep.domStringBuilder();
-  pet.domStringBuilder();
 };
 init();
